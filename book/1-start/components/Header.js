c3026a483e829c910ff1189ffc72cf595478348b
Jonathan White <jonathan42white@gmail.com>
2019-04-08 08:59:58 -0500

    setup env, create context.js, withLayout, header, index, and _document, integrate MUI

---
+++ book/1-start/components/Header.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+
+import { styleToolbar } from './SharedStyles';
+
+const Header = () => (
+  <div>
+    <Toolbar style={styleToolbar}>
+      <Grid container direction="row" justify="space-around" align="center">
+        <Grid item xs={12} style={{ textAlign: 'right' }}>
+          <Link prefetch href="/login">
+            <a style={{ margin: '0px 20px 0px auto' }}>Log in</a>
+          </Link>
+        </Grid>
+      </Grid>
+    </Toolbar>
+  </div>
+);
+foo(reallyLongArg(), reallyReallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());
+export default Header;
+
+
